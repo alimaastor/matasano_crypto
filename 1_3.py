626d9f1c5f7a7f249cb4522d4ac03cee03a51966
alimaastor <alfonsolimaastor@gmail.com>
2015-05-06 15:38:31 +0100

    tidy up 1_3

--- 1_3.py
+++ 1_3.py
@@ -1,17 +1,28 @@
-from Message import Message
-import sys
+
+import argparse
+
+from lib.Message import Message
+
+ciphered_text = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+def main():
+
+    result = []
+
+    for i in xrange(256):
+        key = chr(i)
+        message = Message().set_hex(ciphered_text).xor(key)
+        score = message.score()
+        result.append((score, message, key))
+
+    result.sort(reverse=True)
+
+    print "For a score of [{}], key is [{}] and message is [{}]".format(*result[0])
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Single-byte XOR cipher - Challenge 3 (Set 1) of Matasano Crypto Challenge.')
 
-	result = []
+    args = parser.parse_args()
 
-	for i in xrange(256):
-		key = chr(i)
-		message = Message().set_hex(sys.argv[1]).xor(key)
-		score = message.score()
-		result.append((score, message, key))
-
-	result.sort(reverse=True)
-
-	for r in result:
-		print r
+    main()
