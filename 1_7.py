1f0c34544c8f61d012cd32307c4488143d1d7933
Alfonso Lima Astor <alfonsolimaastor@gmail.com>
2015-05-08 11:00:04 +0100

    tidy up 1_7

--- 1_7.py
+++ 1_7.py
@@ -1,8 +1,18 @@
 
-import sys
+import argparse
+import os
 from Crypto.Cipher import AES
 
+def main():
+    cipher = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
+    with open(os.path.join("data", "1_7_data.txt"), 'r') as f:
+        print "Deciphered text is:\n"
+        print cipher.decrypt(f.read().decode('base64'))
+
 if __name__ == '__main__':
-	cipher = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
-	with open(sys.argv[1], 'r') as f:
-		print cipher.decrypt(f.read().decode('base64'))
+    parser = argparse.ArgumentParser(
+        description='AES in ECB mode - Challenge 7 (Set 1) of Matasano Crypto Challenge.')
+
+    args = parser.parse_args()
+
+    main()
