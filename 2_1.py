3871a65452f69693cbfac76ab2715e393ec4b66d
Alfonso Lima Astor <alfonsolimaastor@gmail.com>
2015-05-08 11:12:28 +0100

    tidy up 2_1

--- 2_1.py
+++ 2_1.py
@@ -1,7 +1,17 @@
 
-import sys
+import argparse
 
 from lib.Message import Message
 
+def main(message):
+    print Message(message).padding(block_length=20)
+
 if __name__ == '__main__':
-	print Message(sys.argv[1]).padding(block_length=20)
+    parser = argparse.ArgumentParser(
+        description='Implement PKCS#7 padding - Challenge 9 (Set 2) of Matasano Crypto Challenge.')
+    parser.add_argument('message',
+        help='Message to be padded.', type=str)
+
+    args = parser.parse_args()
+
+    main(args.message)
