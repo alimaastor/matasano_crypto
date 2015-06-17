cddebc5835da4c82b00f4681e30430f3f54669ed
Alfonso <alfonsolimaastor@gmail.com>
2015-06-17 21:43:50 +0100

    add data and file for 4_1

---
+++ 4_1.py
@@ -0,0 +1,13 @@
+
+import argparse
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Break "random access read/write" AES CTR - Challenge 25 (Set 4) of Matasano Crypto Challenge.')
+
+    args = parser.parse_args()
+
+    main()
