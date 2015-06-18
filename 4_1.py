5ab418c6abb64185cf32a3459646632d71141c71
Alfonso <alfonsolimaastor@gmail.com>
2015-06-18 21:15:58 +0100

    passing 4_1

--- 4_1.py
+++ 4_1.py
@@ -1,6 +1,7 @@
 
 import argparse
 import os
+import base64
 from Crypto.Cipher import AES
 
 from lib.utils import get_passwd
@@ -10,14 +11,16 @@ from lib.Message import Message
 def edit(ciphertext, key, offset, newtext):
     cypher = AES.new(get_passwd(), AES.MODE_CTR, counter=lambda: "\x00" * 16)
     plaintext = cypher.decrypt(ciphertext)
-    for index, newchar in enumerate(newtext):
-        plaintext[offset + index] = newchar
+    plaintext = plaintext[:offset] + newtext + plaintext[offset + len(newtext):]
+    cypher = AES.new(get_passwd(), AES.MODE_CTR, counter=lambda: "\x00" * 16)
     return cypher.encrypt(plaintext)
 
 def main():
     cypher = AES.new(get_passwd(), AES.MODE_CTR, counter=lambda: "\x00" * 16)
     with open(os.path.join('data', '4_1_data.txt'), 'r') as datafile:
-        ciphertext = cypher.encrypt(Message().set_b64(datafile.read().replace('\n', '')).to_str())
+        c = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
+        plaintext = c.decrypt(datafile.read().decode('base64'))
+        ciphertext = cypher.encrypt(plaintext)
 
     assert ciphertext == edit(ciphertext, get_passwd(), 0, '')
 
@@ -28,7 +31,7 @@ def main():
 
     assert len(keystream) == len(ciphertext)
 
-    print Message(ciphertext).xor(keystream).to_str()
+    print Message(ciphertext).xor_whole(keystream).to_str()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
