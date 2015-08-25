7c929dd5e044f887d61b8af9f5fc238016b138d7
Alfonso <alfonsolimaastor@gmail.com>
2015-08-25 21:17:31 +0100

    Server ready for 4_7

--- lib/4_7_server.py
+++ lib/4_7_server.py
@@ -1,35 +1,61 @@
+
 import web
+import hashlib
+import hmac
+import time
 
-urls = ('/upload', 'Upload',
-        '/response', 'Response')
+import utils
 
-class Upload:
+urls = (
+    '/test', 'Test',
+    '/success', 'Success',
+)
+
+timeout = 0.050
+
+def hmac_sha1(key, msg):
+    return hmac.HMAC(key, msg, hashlib.sha1).hexdigest()
+
+def insecure_compare(key1, key2):
+    assert len(key1) == len(key2)
+    key_length = len(key1)
+    for i in xrange(0, key_length, 2):
+        time.sleep(timeout)
+        if key1[i:i+2] != key2[i:i+2]:
+            return False
+    return True
+
+def to_lower_hex_string(key):
+    return str(int(key, 16))
+
+class Test:
+    def GET(self):
+        data = web.input()
+        try:
+            if not (data.file and data.signature):
+                raise web.internalerror()
+        except Exception:
+            raise web.internalerror()
+        else:
+
+            if insecure_compare(
+                    to_lower_hex_string(data.signature),
+                    to_lower_hex_string(hmac_sha1(utils.get_passwd(), data.file))):
+                raise web.seeother('/success')
+            else:
+                raise web.internalerror()
+
+
+class Success:
     def GET(self):
         return """<html><head></head><body>
-<form method="POST" enctype="multipart/form-data" action="" name="uploadFile">
-<input type="file" name="myfile" />
-<br/>
-<input type="submit" />
-</form>
-</body></html>"""
-
-    def POST(self):
-        x = web.input(myfile={})
-        web.debug(x['myfile'].filename) # This is the filename
-        web.debug(x['myfile'].value) # This is the file contents
-        web.debug(x['myfile'].file.read()) # Or use a file(-like) object
-        # raise web.internalerror()
-        raise web.seeother('/response')
-
-class Response:
-    def GET(self):
-        return """<html><head></head><body>
-<h1>hola</h1>
+<h1>Success</h1>
 </body></html>"""
 
 def main():
-   app = web.application(urls, globals())
-   app.run()
+    web.config.debug = False
+    app = web.application(urls, globals())
+    app.run()
 
 
 if __name__ == "__main__":
