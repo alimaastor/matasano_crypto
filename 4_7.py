36811e3dd2565272cfec741b33be5accc4d11b60
Alfonso <alfonsolimaastor@gmail.com>
2015-08-25 21:44:30 +0100

    creating routine to calculate the processing time of signature comparison

--- 4_7.py
+++ 4_7.py
@@ -1,29 +1,47 @@
 
-import os
+import hashlib
+import mechanize
+import time
+
+def try_signature(browser, signature):
+    start_time = time.time()
+    try:
+        browser.open("http://localhost:8080/test?file=foo&signature={}".format(signature))
+    except:
+        elapsed_time = time.time() - start_time
+        return elapsed_time
+
+def get_correct_signature():
+    signature = '0' * hashlib.sha1().block_size
+
+    # Create browser and configure.
+    browser = mechanize.Browser()
+    browser.set_handle_equiv(True)
+    browser.set_handle_redirect(True)
+    browser.set_handle_referer(True)
+    browser.set_handle_robots(False)
+    browser.set_handle_robots( False )
+    browser.addheaders = [('User-agent', 'Firefox')]
+
+    # First we get the minimum processing time.
+    elapsed_time = try_signature(browser, signature[:2])
+    print 'minimum time:', elapsed_time
+    exit()
+
+    while True:
+        start_time = time.time()
+
+        # Retrieve the page, saving the response
+        br.open("http://localhost:8080/test")
+        br._factory.is_html = True
+        print br.response().code
+
+        elapsed_time = time.time() - start_time
 
 def main():
+    get_correct_signature()
+    
     import re
-    import mechanize
-    from mechanize import Browser
-
-    br = Browser()
-
-    # Browser options
-    br.set_handle_equiv(True)
-    br.set_handle_redirect(True)
-    br.set_handle_referer(True)
-    br.set_handle_robots(False)
-
-    # Ignore robots.txt
-    br.set_handle_robots( False )
-    # Google demands a user-agent that isn't a robot
-    br.addheaders = [('User-agent', 'Firefox')]
-
-    # Retrieve the Google home page, saving the response
-    br.open( "http://localhost:8080/upload" )
-    br._factory.is_html = True
-    print br.response().read()
-    print br.response().code
 
     # Select the search box and search for 'foo'
     br.select_form( 'uploadFile' )
