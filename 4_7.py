3b3279ab64cfee1426eb06f430ba1cd7d9afafa2
Alfonso Lima <alfonsolimaastor@gmail.com>
2015-09-16 00:40:08 +0100

    First solution to 4_7

--- 4_7.py
+++ 4_7.py
@@ -2,8 +2,33 @@
 import hashlib
 import mechanize
 import time
+import subprocess
+import os
+import copy
+
+import lib.utils as utils
+
+# Length of the signature in hex string.
+SIGNATURE_LEN = 40
+
+class SignatureFound(Exception):
+    '''When the correct signature is found, the program raises this exception to signal it and exit the search.'''
+    pass
+
+
+def config_browser():
+    '''Returns Browers instance fully configured.'''
+    browser = mechanize.Browser()
+    browser.set_handle_equiv(True)
+    browser.set_handle_redirect(True)
+    browser.set_handle_referer(True)
+    browser.set_handle_robots(False)
+    browser.set_handle_robots(False)
+    browser.addheaders = [('User-agent', 'Firefox')]
+    return browser
 
 def try_signature(browser, signature):
+    '''Returns the total time it took to check the signature or None if signature was correct.'''
     start_time = time.time()
     try:
         browser.open("http://localhost:8080/test?file=foo&signature={}".format(signature))
@@ -11,55 +36,96 @@ def try_signature(browser, signature):
         elapsed_time = time.time() - start_time
         return elapsed_time
 
-def get_correct_signature():
-    signature = '0' * hashlib.sha1().block_size
-    print 'Initial signature:', signature
+def rjust_signature(signature):
+    return signature.ljust(SIGNATURE_LEN, '0')
 
-    # Create browser and configure.
-    browser = mechanize.Browser()
-    browser.set_handle_equiv(True)
-    browser.set_handle_redirect(True)
-    browser.set_handle_referer(True)
-    browser.set_handle_robots(False)
-    browser.set_handle_robots( False )
-    browser.addheaders = [('User-agent', 'Firefox')]
+@utils.static_var('comms_latency', None)
+def is_previous_guess_wrong(timeouts, signature, server_timeout):
+    '''Checks whether we should revert back to the previous guess because no new byte causes delay to be bigger.'''
 
-    # First we get the minimum processing time.
-    elapsed_time = try_signature(browser, signature[:])
-    print 'minimum time:', elapsed_time
-    exit()
+    # We get all but the greatest 5 times.
+    all_times = copy.copy(timeouts.values())
+    sorted(all_times)
+    all_times_filtered = all_times[:-5]
+    avg_time = sum(all_times_filtered) / len(all_times_filtered)
 
-    while True:
-        start_time = time.time()
+    # If we are guessing the first byte, we have to calculate the communitations latency.
+    if not signature and not is_previous_guess_wrong.comms_latency:
+        is_previous_guess_wrong.comms_latency = avg_time - server_timeout
+        print 'setting comms_latency to', is_previous_guess_wrong.comms_latency
 
-        # Retrieve the page, saving the response
-        br.open("http://localhost:8080/test")
-        br._factory.is_html = True
-        print br.response().code
+    # this is the minimum time every comparison should take, otherwise we have previously guessed some incorrect bytes.
+    minimum_timeout = (len(signature) + 1) * server_timeout + is_previous_guess_wrong.comms_latency
 
-        elapsed_time = time.time() - start_time
+    if avg_time < minimum_timeout:
+        print 'avg_time greater than server_tiumeout'
+        return True
+    else:
+        return False
+
+def get_next_byte_timeouts(browser, signature):
+    timeouts = {}
+    for value in xrange(0,256):
+        candidate = hex(value)[2:].rjust(2, '0')
+        candidate_signature = rjust_signature(''.join(signature) + candidate)
+        timeout = try_signature(browser, candidate_signature)
+        if not timeout:
+            print len(signature), SIGNATURE_LEN, len(signature) == (SIGNATURE_LEN / 2 - 1)
+            signature.append(candidate)
+            raise SignatureFound()
+        timeouts[candidate] = try_signature(browser, candidate_signature)
+    return timeouts
+
+def get_best_candidate(timeouts):
+    return max(timeouts.items(), key=lambda x: x[1])
+
+def find_signature(browser, server_timeout):
+    '''Returns signature in hex string.'''
+    signature = []
+    try:
+        # Iterate until the signature is found or an error occur.
+        while True:
+            # First, we get the processing times for all possible options.
+            timeouts = get_next_byte_timeouts(browser, signature)
+
+            # Now we check whether something has gone wrong and our previous guess is wrong.
+            if is_previous_guess_wrong(timeouts, signature, server_timeout):
+                signature = signature[:-1]
+            else:
+                print 'best candidate is', get_best_candidate(timeouts)
+                for i in timeouts.iteritems():
+                    print i
+                print '*'*50
+                signature.append(get_best_candidate(timeouts)[0])
+                print "signature is", signature
+
+    except SignatureFound:
+        print "Signature has been found:", signature
+        return ''.join(signature)
 
 def main():
-    get_correct_signature()
-    
-    import re
+    # Init variables.
+    os.environ['SERVER_TIMEOUT'] = "0.0050"
+    server_timeout = float(os.environ['SERVER_TIMEOUT'])
 
-    # Select the search box and search for 'foo'
-    br.select_form( 'uploadFile' )
-    filename = os.path.join('data', '4_7_data.txt')
-    br.form.add_file(open(filename), 'text/plain', filename)
+    # Create browser and configure.
+    browser = config_browser()
 
-    # Get the search results
-    try:
-        br.submit()
-    except Exception:
-        print '500'
-    else:
-        print '200'
+    # Find signature.
+    signature = find_signature(browser, server_timeout)
+
+    assert len(signature) == SIGNATURE_LEN, "Signature not found! {} != {}".format(len(signature), SIGNATURE_LEN)
+    print 'signature is', signature
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
-        description='Implement and break HMAC-SHA1 with an artificial timing leak - Challenge 31 (Set 4) of Matasano Crypto Challenge.')
+        description=('Implement and break HMAC-SHA1 with an artificial '
+                     'timing leak - Challenge 31 (Set 4) of Matasano Crypto Challenge.'))
 
-    main()
+    args = parser.parse_args()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
--- lib/timing_leak__server.py
+++ lib/timing_leak__server.py
@@ -3,6 +3,7 @@ import hashlib
 import hmac
 import time
 import sys
+import os
 
 try:
     import web
@@ -19,7 +20,7 @@ urls = (
 
 
 class Timeout(object):
-    timeout = 0.050
+    timeout = float(os.environ.get('SERVER_TIMEOUT', 0.0050))
 
     @staticmethod
     def get_timeout():
@@ -27,6 +28,7 @@ class Timeout(object):
 
     @staticmethod
     def set_timeout(new_timeout):
+        assert isinstance(new_timeout, float)
         Timeout.timeout = new_timeout
 
 
@@ -34,6 +36,8 @@ def hmac_sha1(key, msg):
     return hmac.HMAC(key, msg, hashlib.sha1).hexdigest()
 
 def insecure_compare(key1, key2):
+    print key1
+    print key2
     assert len(key1) == len(key2), "{} != {}".format(len(key1), len(key2))
     key_length = len(key1)
     for i in xrange(0, key_length, 2):
@@ -43,7 +47,7 @@ def insecure_compare(key1, key2):
     return True
 
 def to_lower_hex_string(key):
-    return str(int(key, 16)).zfill(hashlib.sha1().block_size)
+    return hex(int(key, 16))[2:].replace('L', '').zfill(40)
 
 
 class Test:
@@ -58,7 +62,7 @@ class Test:
 
             if insecure_compare(
                     to_lower_hex_string(data.signature),
-                    to_lower_hex_string(hmac_sha1(utils.get_passwd(), data.file))):
+                    hmac_sha1(utils.get_passwd(), data.file)):
                 raise web.seeother('/success')
             else:
                 raise web.internalerror()
@@ -78,4 +82,13 @@ def main():
 
 if __name__ == "__main__":
 
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description='Server to practice timing leak attacks.')
+
+    args = parser.parse_args()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+
