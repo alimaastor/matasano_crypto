e54ed223ab08f67c90c509d1fcab26176957f13f
Alfonso Lima <alfonsolimaastor@gmail.com>
2015-10-04 18:38:26 +0100

    Update 5_4 to use sockets and srp_server

--- 5_4.py
+++ 5_4.py
@@ -1,42 +1,27 @@
 
+import socket
 import hashlib
 import hmac
+import json
+import subprocess
+import time
+import os
 
 from lib.Message import Message
 from lib.utils import crypto_random
+from lib.socket_wrapper import socket_send, socket_receive, get_client_socket
 
-def main(args):
+def log_in(sock):
     # C & S: Agree on N=[NIST Prime], g=2, k=3, I (email), P (password)
     N = 0x009f4f57c0d386b90c5cf147d96466c5c7b2d154e7d32a58695191847f58f5e2ca9c28b497ae1b31d1c2507b1c489662a9d39c5b5100503888cfd7c762a7c1310d26b8ae38ad8de4ba3ff800022221c73be2da3113b4a7ba404a32a446adb9dedb2958bab3b26f2984396e1af1fc28594182b2a72de7fed99ea03e34c0d69e02db
     g = 2
     k = 3
     I = 'example@mail.com'
     P = 'somepassword1234'
-    print "C & S: Agree on N={}, g={}, k={}, I ({}), P ({})".format(
-        N, g, k, I, P)
-    print ""
 
-    # S:
-    print "S:"
-
-    #   Generate salt as random integer
-    salt = crypto_random(32)
-    salt_str = Message().set_int(salt).to_str()
-    print "  Generate salt as random integer:", salt
-
-    #   Generate string xH=SHA256(salt|password)
-    #   Convert xH to integer x somehow (put 0x on hexdigest)
-    x_sha256_generator = hashlib.sha256()
-    x_sha256_generator.update(salt_str + P)
-    x = int(x_sha256_generator.hexdigest(), 16)
-    print "  Generate string xH=SHA256(salt|password)"
-    print "  Convert xH to integer x somehow (put 0x on hexdigest):", x
-
-    #   Generate v=g**x % N
-    #   Save everything but x, xH
-    v = pow(g, x, N)
-    print "  Generate v=g**x % N:", v
-    print "  Save everything but x, xH"
+    print "Sending N, g & k to the server."
+    data = json.dumps({'N': N, 'g': g, 'k': k})
+    socket_send(sock, data)
     print ""
 
     # C->S: Send I, A=g**a % N (a la Diffie Hellman)
@@ -47,11 +32,16 @@ def main(args):
     print "A:", A
     print ""
 
-    # S->C: Send salt, B=kv + g**b % N
-    print "S->C: Send salt, B=kv + g**b % N"
-    print "salt", salt
-    b = crypto_random(32)
-    B = k * v + pow(g, b, N)
+    data = json.dumps({'I': I, 'A': A})
+    socket_send(sock, data)
+
+    values = socket_receive(sock)
+    for key, value in values.iteritems():
+        globals()[key] = value
+    assert salt
+    assert B
+    print 'Received variables salt and B'
+    print "salt:", salt
     print "B:", B
     print ""
 
@@ -68,53 +58,73 @@ def main(args):
     #   Convert xH to integer x somehow (put 0x on hexdigest)
     print "  Generate string xH=SHA256(salt|password)"
     print "  Convert xH to integer x somehow (put 0x on hexdigest)"
+    x_sha256_generator = hashlib.sha256()
+    x_sha256_generator.update(Message().set_int(salt).to_str() + P)
+    x = int(x_sha256_generator.hexdigest(), 16)
     #   Generate S = (B - k * g**x)**(a + u * x) % N
     print "  Generate S = (B - k * g**x)**(a + u * x) % N"
     # buff = B - k * g**x
-    S_c = pow(B - k * pow(g, x, N), a + u * x, N)
-    print "S:", S_c
+    S = pow(B - k * pow(g, x, N), a + u * x, N)
+    print "S:", S
     #   Generate K = SHA256(S)
     print "  Generate K = SHA256(S)"
-    K_c_sha256_generator = hashlib.sha256()
-    K_c_sha256_generator.update(Message().set_int(S_c).to_str())
-    K_c = int(K_c_sha256_generator.hexdigest(), 16)
-    print "K:", K_c
-    print ""
-
-    # S:
-    #   Generate S = (A * v**u) ** b % N
-    print "  Generate S = (A * v**u) ** b % N"
-    S_s = pow(A * pow(v, u, N), b, N)
-    print "S:", S_s
-    #   Generate K = SHA256(S)
-    print "  Generate K = SHA256(S)"
-    K_s_sha256_generator = hashlib.sha256()
-    K_s_sha256_generator.update(Message().set_int(S_s).to_str())
-    K_s = int(K_s_sha256_generator.hexdigest(), 16)
-    print "K:", K_s
+    K_sha256_generator = hashlib.sha256()
+    K_sha256_generator.update(Message().set_int(S).to_str())
+    K = int(K_sha256_generator.hexdigest(), 16)
+    print "K:", K
     print ""
 
     # C->S: Send HMAC-SHA256(K, salt)
-    signature_c = hmac.HMAC(Message().set_int(K_c).to_str(), salt_str, hashlib.sha256).hexdigest()
+    signature = hmac.HMAC(Message().set_int(K).to_str(), Message().set_int(salt).to_str(), hashlib.sha256).hexdigest()
+    data = json.dumps({'signature': signature})
+    socket_send(sock, data)
     print "C->S: Send HMAC-SHA256(K, salt)"
-    print "HMAC-SHA256(K, salt):", signature_c
     print ""
 
-    # S->C: Send "OK" if HMAC-SHA256(K, salt) validates
-    signature_s = hmac.HMAC(Message().set_int(K_s).to_str(), salt_str, hashlib.sha256).hexdigest()
-    print "S->C: Send \"OK\" if HMAC-SHA256(K, salt) validates"
+    values = socket_receive(sock)
+    for key, value in values.iteritems():
+        globals()[key] = value
+    assert response
+    if response == "OK":
+        print "OK: Logged in"
+    elif response == 'ERROR':
+        print "ERROR: Not logged in"
+
+def main(args):
+    # Start server.
+    print "Starting the server ..."
+    server_path = os.path.join('lib', 'srp_server.py')
+    server_logfile = os.path.join("/tmp", "srp_server.log")
+    with open(server_logfile, 'w+') as devnull:
+        server_process = subprocess.Popen(
+            'python {} {}'.format(server_path, str(args.port_number)),
+            shell=True,
+            stdout=devnull,
+            stderr=devnull)
+    time.sleep(2)
+    ret = server_process.poll()
+    if ret != None:
+        print "Server has not started correctly. Port already in use?"
+        exit(1)
+
+    # Get communication socket.
+    sock = get_client_socket(args.hostname, args.port_number)
+    print "Communications initialised"
+    print ""
     try:
-        assert signature_s == signature_c
-    except AssertionError:
-        print "ERROR: Signatures are not equal!!"
-    else:
-        print "OK: Signature validates"
+        log_in(sock)
+    finally:
+        sock.shutdown(socket.SHUT_RDWR)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
         description='Implement Secure Remote Password (SRP) - Challenge 35 (Set 5) of Matasano Crypto Challenge.'
     )
+    parser.add_argument('hostname',
+        help='Address of the host to connect to', type=str)
+    parser.add_argument('port_number',
+        help='Port number to connect to', type=int)
     args = parser.parse_args()
 
     main(args)
